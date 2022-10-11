7dd7df45ea44e81423eb047be2458dc5439fef2c
Ookenfooken <40247563+Ookenfooken@users.noreply.github.com>
2022-10-11 12:33:57 -0400

    finalized glm
    cleaned up analysis

--- R/GLMmovementPhases.R
+++ R/GLMmovementPhases.R
@@ -7,40 +7,40 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 ### SEPARATE BALL/SLOT PHASES ###
 #################################
 ballFixData = data.frame(readMat("glmData_ball.mat"))
-colnames(ballFixData) <- c("participant", "testID", "ballFix", "reachOnset", "reachPeakVel", 
-                           "ballApproach", "ballGrasp", "transport", "transportPeakVel", 
-                           "slotApproach", "slotEntry", "fixationOnset", 
-                           "fixationOffset", "fixationMidpoint")
+colnames(ballFixData) <- c("participant", "testID", "ballFix", "reachOnset", 
+                           "ballApproach", "ballGrasp", "transport", 
+                           "slotApproach", "slotEntry", 
+                           "fixationOnset", "fixationOffset")
 ballFixData$participant <- as.factor(ballFixData$participant)
 ballFixData$testID <- as.factor(ballFixData$testID)
 
 ballFixData$ballFix <- NULL
 ballFixations_FT <- ballFixData[ballFixData$testID == 3,] # fingertips
 ballFixations_TW <- ballFixData[ballFixData$testID == 4,] # tweezers
-ballFix.FT.onset <- lmer(fixationOnset ~ reachOnset + reachPeakVel + ballApproach + ballGrasp + transport +
-                       transportPeakVel + slotApproach + slotEntry + (1|participant), data = ballFixations_FT)
-ballFix.TW.onset <- lmer(fixationOnset ~  reachOnset + reachPeakVel + ballApproach + ballGrasp + transport +
-                       transportPeakVel + slotApproach + slotEntry + (1|participant), data = ballFixations_TW)
+ballFix.FT.onset <- lmer(fixationOnset ~ reachOnset + ballApproach + ballGrasp + transport +
+                       slotApproach + slotEntry + (1|participant), data = ballFixations_FT)
+ballFix.TW.onset <- lmer(fixationOnset ~  reachOnset + ballApproach + ballGrasp + transport +
+                       slotApproach + slotEntry + (1|participant), data = ballFixations_TW)
 ballFix.FT.offset <- lmer(fixationOffset ~ reachOnset + reachPeakVel + ballApproach + ballGrasp + transport +
                            transportPeakVel + slotApproach + slotEntry + (1|participant), data = ballFixations_FT)
 ballFix.TW.offset <- lmer(fixationOffset ~  reachOnset + reachPeakVel + ballApproach + ballGrasp + transport +
                            transportPeakVel + slotApproach + slotEntry + (1|participant), data = ballFixations_TW)
 
 slotFixData = data.frame(readMat("glmData_slot.mat"))
-colnames(slotFixData) <- c("participant", "testID", "slotFix", "reachOnset", "reachPeakVel", 
-                           "ballApproach", "ballGrasp", "transport", "transportPeakVel", 
-                           "slotApproach", "slotEntry", "fixationOnset", 
-                           "fixationOffset", "fixationMidpoint")
+colnames(slotFixData) <- c("participant", "testID", "slotFix", "reachOnset", 
+                           "ballApproach", "ballGrasp", "transport", 
+                           "slotApproach", "slotEntry", 
+                           "fixationOnset", "fixationOffset")
 slotFixData$participant <- as.factor(slotFixData$participant)
 slotFixData$testID <- as.factor(slotFixData$testID)
 
 slotFixData$slotFix <- NULL
 slotFixations_FT <- slotFixData[slotFixData$testID == 3,] # fingertips
 slotFixations_TW <- slotFixData[slotFixData$testID == 4,] # tweezers
-slotFix.FT.onset <- lmer(fixationOnset ~ reachOnset + reachPeakVel + ballApproach + ballGrasp + transport +
-                         transportPeakVel + slotApproach + slotEntry + (1|participant), data = slotFixations_FT)
-slotFix.TW.onset <- lmer(fixationOnset ~ reachOnset + reachPeakVel + ballApproach + ballGrasp + transport +
-                         transportPeakVel + slotApproach + slotEntry + (1|participant), data = slotFixations_TW)
+slotFix.FT.onset <- lmer(fixationOnset ~ reachOnset + ballApproach + ballGrasp + transport +
+                         slotApproach + slotEntry + (1|participant), data = slotFixations_FT)
+slotFix.TW.onset <- lmer(fixationOnset ~ reachOnset + ballApproach + ballGrasp + transport +
+                         slotApproach + slotEntry + (1|participant), data = slotFixations_TW)
 slotFix.FT.offset <- lmer(fixationOffset ~ reachOnset + reachPeakVel + ballApproach + ballGrasp + transport +
                            transportPeakVel + slotApproach + slotEntry + (1|participant), data = slotFixations_FT)
 slotFix.TW.offset <- lmer(fixationOffset ~ reachOnset + reachPeakVel + ballApproach + ballGrasp + transport +
--- analyzeFixationSubGoals.m
+++ analyzeFixationSubGoals.m
@@ -19,21 +19,17 @@ for j = 1:numParticipants % loop over subjects
         participant = currentParticipant*ones(numTrials, 1);
         testID = i*ones(numTrials,1);
         reachOnset = NaN(numTrials,1);
-        reachPeakVel = NaN(numTrials,1);
         ballApproach = NaN(numTrials,1);
         ballGrasp = NaN(numTrials,1);
         transport = NaN(numTrials,1);
-        transportPeakVel = NaN(numTrials,1);
         slotApproach = NaN(numTrials,1);
         slotEntry = NaN(numTrials,1);
         ballFixations = zeros(numTrials,1);
         ballOnsets = NaN(numTrials,1);
         ballOffsets = NaN(numTrials,1);
-        ballMidpoint = NaN(numTrials,1);
         slotFixation = zeros(numTrials,1);
         slotOnsets = NaN(numTrials,1);
         slotOffsets = NaN(numTrials,1);
-        slotMidpoint = NaN(numTrials,1);
         for n = 1:stopTrial % loop over trials for current subject & block
             if currentResult(n).info.dropped
                 stopTrial = min([stopTrial+1 numTrials]);
@@ -49,19 +45,15 @@ for j = 1:numParticipants % loop over subjects
                 continue
             end
             reachOnset(n) = currentResult(n).info.phaseStart.primaryReach ;
-            reachPeakVel(n) = find(toolVelocity(startFrame:stopFrame) == max(toolVelocity(startFrame:stopFrame))) + trialStart;
             ballApproach(n) = currentResult(n).info.phaseStart.ballApproach;
             ballGrasp(n) = currentResult(n).info.phaseStart.ballGrasp;
             transport(n) = startTransport;
-            transportPeakVel(n) = startTransport + find(toolVelocity(stopFrame:currentResult(n).info.phaseStart.slotApproach-trialStart)...
-                == max(toolVelocity(stopFrame:currentResult(n).info.phaseStart.slotApproach-trialStart)), 1, 'first');
             slotApproach(n) = currentResult(n).info.phaseStart.slotApproach;
             slotEntry(n) = currentResult(n).info.phaseStart.ballInSlot;
             if ~isempty(currentResult(n).gaze.fixation.onsetsBall)
                 ballFixations(n) = 1;
                 ballOnsets(n) = currentResult(n).gaze.fixation.onsetsBall(1)+trialStart;
                 ballOffsets(n) = currentResult(n).gaze.fixation.offsetsBall(1)+trialStart;
-                ballMidpoint(n) = ballOnsets(n) + (ballOffsets(n) - ballOnsets(n))/2;
                 if numel(currentResult(n).gaze.fixation.onsetsBall)>1
                     ballFixations(n) = 2;
                 end
@@ -82,43 +74,42 @@ for j = 1:numParticipants % loop over subjects
                     slotOnsets(n) = currentResult(n).gaze.fixation.onsetsSlot+trialStart;
                     slotOffsets(n) = currentResult(n).gaze.fixation.offsetsSlot+trialStart;
                 end
-                slotMidpoint(n) = slotOnsets(n) + (slotOffsets(n) - slotOnsets(n))/2;
             end
             clear toolVelocity slotIndx startFrame stopFrame trialStart
         end
         currentVariable = [participant testID reachOnset ...
-            reachPeakVel ballApproach ballGrasp transport transportPeakVel slotApproach slotEntry ...
-            ballFixations ballOnsets ballOffsets ballMidpoint ...
-            slotFixation slotOnsets slotOffsets slotMidpoint];
+            ballApproach ballGrasp transport slotApproach slotEntry ...
+            ballFixations ballOnsets ballOffsets ...
+            slotFixation slotOnsets slotOffsets ];
         
         variables = [variables; currentVariable];
         
     end
 end
 %% separate data into conditions
-ballFixations = [variables(:,1:2) variables(:,11) variables(:,3:10) variables(:,12:14)]; % paste relevant ball data
+ballFixations = [variables(:,1:2) variables(:,9) variables(:,3:8) variables(:,10:11)]; % paste relevant ball data
 ballFixations = ballFixations(ballFixations(:,3) > 0, :);
 ballFixations_adjusted = [];
 for tool = 3:4
     currentTool = ballFixations(ballFixations(:,2) == tool, :);
     for i = 1:numParticipants
         currentParticipant = currentTool(currentTool(:,1) == i, :);
-        currentPhases = currentParticipant(:,4:11) - mean(currentParticipant(:,4:11));
+        currentPhases = currentParticipant(:,4:9) - mean(currentParticipant(:,4:9));
         ballFixations_adjusted = [ballFixations_adjusted; ...
-            [currentParticipant(:,1:3) currentPhases currentParticipant(:,12:14)]];
+            [currentParticipant(:,1:3) currentPhases currentParticipant(:,10:11)]];
     end
 end
 
-slotFixations = [variables(:,1:2) variables(:,15) variables(:,3:10) variables(:,16:18)]; % paste relevatn slot data
+slotFixations = [variables(:,1:2) variables(:,12) variables(:,3:8) variables(:,13:14)]; % paste relevatn slot data
 slotFixations = slotFixations(slotFixations(:,3) > 0, :);
 slotFixations_adjusted = [];
 for tool = 3:4
     currentTool = slotFixations(slotFixations(:,2) == tool, :);
     for i = 1:numParticipants
         currentParticipant = currentTool(currentTool(:,1) == i, :);
-        currentPhases = currentParticipant(:,4:11) - mean(currentParticipant(:,4:11));
+        currentPhases = currentParticipant(:,4:9) - mean(currentParticipant(:,4:9));
         slotFixations_adjusted = [slotFixations_adjusted; ...
-            [currentParticipant(:,1:3) currentPhases currentParticipant(:,12:14)]];
+            [currentParticipant(:,1:3) currentPhases currentParticipant(:,10:11)]];
     end
 end
 %%
